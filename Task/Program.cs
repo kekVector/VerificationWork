eb4970f26eb540b30a3e69709eb1ce6dabb60d22
KekVector <k.o.email@yandex.ru>
2021-12-28 16:34:30 +0300

    добавил стандартные методы для работы с массивами. Print, Fill, Create

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1, b = 20;
+int n = 15;
+
+int[] CreatArray(int N)
+{
+    return new int[N];
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine(String.Join(' ', arr));
+}
+
+int[] FillArray(int[] arr, int A, int B)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(A, B+1);        
+    }
+    return arr;
+}
+int[] array = FillArray(CreatArray(n), a, b);
+PrintArray(array);
+
